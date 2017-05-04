10c6359803741067f8ea39a4e5d398d4668355cf
Gary Kertis <gary.kertis@gmail.com>
2017-05-03 20:11:47 -0400

    Change login callbacks.

--- src/server/config/auth.js
+++ src/server/config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1172072612858943', // your App ID
         'clientSecret'  : '7275f7b2d137eace05968897b427d10b', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL'   : 'http://quvl.io/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'your-consumer-key-here',
         'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
+        'callbackURL'       : 'http://quvl.io/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '344130109996-epam45tq45so016d5knbar0insv8qvfv.apps.googleusercontent.com',
         'clientSecret'  : '-Xc4ufVOs5-cD6j2s5PVY9bV',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'http://quvl.io/auth/google/callback'
     }
 
 };
